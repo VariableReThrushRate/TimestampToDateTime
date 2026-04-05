035d03cc855cedf643f4feb2397abf81822c8151
VariableReThrushRate <bgpilot16@gmail.com>
2026-04-05 11:53:08 -0700

    Updated gitignore, fixed a typo

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ print(sourcedir)
 print(outputdir)
 
 # To be Done
-# Get input scheme from previous scale project for source and output direetories
+# Get input scheme from previous scale project for source and output diretories
 # Make a list by making parsed datetime objects out of each file in the directory (do as same time as dict)
 # Make a dictionary that caches the file name associated with the datetime object, datetime as key because I'm insane
 # For loop that iterates over the list in order and outputs the files with the kdenlive format.
